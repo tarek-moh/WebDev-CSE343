143c3c00bedcc1940a23b248a497cea11410deac
tarek-moh <tarekmoh5557@gmail.com>
2025-11-18 20:33:41 +0200

    Navbar

---
+++ udemy-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from '/src/Navbar/Navbar.jsx'
+import LoginLanding from '/src/LoginLanding/LoginLanding.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <LoginLanding />
+    </>
+  )
+}
+
+export default App
